628b8f60c2bbe5c1f02ee5d382969027e11bf640
Alena Bobrova <ebobrova636@gmail.com>
2025-10-07 17:15:33 +0300

    delete token

--- tests/getClubs.test.ts
+++ tests/getClubs.test.ts
@@ -7,7 +7,7 @@ test.describe("API-тесты на получения списка клубов"
             `${api.urls.base_url_api}${api.paths.clubs}`,
             {
                 headers: {
-                    'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJtb2JpbGVfYWRtaW4iLCJlbXBsb3llZV9pZCI6MTAyMTQsImV4cCI6MTc1OTU3MDc3OCwiaWF0IjoxNzU5NDg0Mzc4LCJpc3MiOiJvYXV0aC0wLjEuMC0wLjEuMSIsImppZCI6Ijc5MDIzMTU1MjMzQGNoYXQtdGVzdC5kZHhmaXRuZXNzLnJ1IiwianRpIjoiNTQ4NTEwYjUtYTA0OS00OTFhLTg1YTMtODM0ZTU0NWYyZDcwIiwibmJmIjoxNzU5NDg0Mzc4LCJwZXJtaXNzaW9ucyI6W10sInJvb21famlkIjoiNzkwMjMxNTUyMzNAY29uZmVyZW5jZS5jaGF0LXRlc3QuZGR4Zml0bmVzcy5ydSIsInN1YiI6InBhc3N3b3JkIiwidXNlcl9pZCI6Mjc3Mzc4N30.6wYSssMLcqfLz-WSbPguYMApBeJDm8LZvr0k3qlJn8c"
+                    'Authorization': api.tokens.test,
                 },
                 params: {
                     session_id: "1212",
@@ -20,12 +20,12 @@ test.describe("API-тесты на получения списка клубов"
         expect(response.status()).toEqual(200);
     });
 
-    test("[negative] получить список клубов с неверным токеном", async ({ request }) => {
+    test("[negative] получить список клубов без обязательного параметра", async ({ request }) => {
         const response = await request.get(
             `${api.urls.base_url_api}${api.paths.clubs}`,
             {
                 headers: {
-                    Authorization: "",
+                    'Authorization': api.tokens.test,
                 },
                 params: {
                     session_id: "1212",
